cb849364f088ddd5f98670d1ea7a463b7ee17982
Aleksandra Henig <Ahenig@finitec.pl>
2022-09-15 16:54:52 +0200

    added git config to adding tag

--- .github/scripts/tag.sh
+++ .github/scripts/tag.sh
@@ -4,4 +4,6 @@ set -e
 latestTag=$(git describe --tags --abbrev=0 --match $1*)
 tagComponents=(${latestTag//-/ })
 incrementedNumber=$((${tagComponents[1]}+1))
+git config user.name "GitHub Actions Bot"
+git config user.email "<>"
 git tag -a $1$incrementedNumber -m "Deployment $1$incrementedNumber"

b44d2b1bb3ba860579aea4e717f8e9aef874f2d6
Aleksandra Henig <Ahenig@finitec.pl>
2022-10-21 16:16:55 +0200

    ABC-110 small changelog output changes

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -1,10 +1,11 @@
 oldCommit=$1
 newCommit=$2
 
-echo 'User Stories: '
+echo '::::CHANGELOG'
+echo '::User Stories: '
 git log $oldCommit..$newCommit --no-merges --pretty=format:"%h - %ae, %s" | grep -oP --regexp="($3-\d+)" | sort -r | uniq
 echo ''
-echo 'Commits: '
+echo '::Commits: '
 echo '{Commit hash} – {Author email} – {Commit Message}'
 echo ''
  

edf7f34056630701d92c8ca0a55a1411341b8b01
Aleksandra Henig <Ahenig@finitec.pl>
2022-11-17 14:10:11 +0100

    changelog changes

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -1,6 +1,11 @@
 oldCommit=$1
 newCommit=$2
 
+echo ':::: DEPLOYMENT INFO'
+echo ':: Current commit: ' + ${ git show --format="%H by %an, %as (%ar)" --no-patch }
+echo ':: Current branch: ' + ${ git rev-parse --abbrev-ref HEAD }
+echo ''
+
 echo ':::: CHANGELOG'
 echo ':: User Stories: '
 git log $oldCommit..$newCommit --no-merges --pretty=format:"%h - %ae, %s" | grep -oP --regexp="($3-\d+)" | sort -r | uniq
